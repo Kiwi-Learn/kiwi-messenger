7ced661c7009ef5abef6a16d2faa228b7174cf28
Che-Wei Lin <linton.tw@gmail.com>
2015-12-19 18:23:11 +0800

    using get_queue_url to get queuee url

--- publisher.rb
+++ publisher.rb
@@ -4,9 +4,13 @@ require 'aws-sdk'
 ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
 sqs = Aws::SQS::Client.new()
 
+q_url = sqs.get_queue_url(queue_name: 'Kiwi_messenger').queue_url
+
+msg = {results: 'Hello World at' + Time.now.to_s}
+
 resp = sqs.send_message({
-  queue_url: ENV['QUEUE_URL'], # required
-  message_body: 'Hello World at' + Time.now.to_s # required
+  queue_url: q_url, # required
+  message_body: msg.to_json # required
 })
 
 if resp.successful?
