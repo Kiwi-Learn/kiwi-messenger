39c742ff4c97a2b04f5b80ceeddb4d68f9ccbb47
Che-Wei Lin <linton.tw@gmail.com>
2015-12-19 18:24:27 +0800

    using get_queue_url to get queuee url

--- subscriber.rb
+++ subscriber.rb
@@ -4,11 +4,14 @@ require 'aws-sdk'
 ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
 sqs = Aws::SQS::Client.new()
 
-resp = sqs.receive_message({queue_url: ENV['QUEUE_URL']})
+q_url = sqs.get_queue_url(queue_name: 'Kiwi_messenger').queue_url
 
-if not resp.messages.empty?
+resp = sqs.receive_message({queue_url: q_url})
+msg = resp.messages.first
+
+if msg
   sqs.delete_message({
-    queue_url: ENV['QUEUE_URL'],
+    queue_url: q_url,
     receipt_handle: resp.messages[0].receipt_handle
   })
   puts 'pop out message: ' + resp.messages[0].body
